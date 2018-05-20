799660865810e449bcb779f1e26cd799b99d4fee
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2018-05-20 14:18:18 -0700

    Adding check_cycle Function

--- 0x00-linked_list_cycle/0-check_cycle.c
+++ 0x00-linked_list_cycle/0-check_cycle.c
@@ -1,7 +1,20 @@
 #include "lists.h"
+#include <stdio.h>
+#define NOCYCLE 0
+#define CYCLE 1
 
+/**
+  * check_cycle - Checks singly linked lists for cycles
+  * @list: Singly linked list
+  * Return: 0 if no cycle, 1 if cycle
+  **/
 int check_cycle(listint_t *list)
 {
-	list++;
-	return (0);
+	listint_t *head;
+
+	head = list;
+	while ((head = head->next) != NULL)
+		if ((head->next) == list)
+			return (CYCLE);
+	return (NOCYCLE);
 }
