b2741cf148a83edadeff8f9cd2e9cbc0f2087f22
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2018-05-28 16:26:11 -0700

    Clean up repo

--- 0x00-linked_list_cycle/src/0-linked_lists.c
+++ 0x00-linked_list_cycle/src/0-linked_lists.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include "lists.h"
 
+
 /**
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
@@ -9,19 +10,19 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    const listint_t *current;
-    unsigned int n; /* number of nodes */
+	const listint_t *current;
+	unsigned int n; /* number of nodes */
 
-    current = h;
-    n = 0;
-    while (current != NULL)
-    {
-        printf("%i\n", current->n);
-        current = current->next;
-        n++;
-    }
+	current = h;
+	n = 0;
+	while (current != NULL)
+	{
+		printf("%i\n", current->n);
+		current = current->next;
+		n++;
+	}
 
-    return (n);
+	return (n);
 }
 
 /**
@@ -32,17 +33,17 @@ size_t print_listint(const listint_t *h)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-    listint_t *new;
+	listint_t *new;
 
-    new = malloc(sizeof(listint_t));
-    if (new == NULL)
-        return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
 
-    new->n = n;
-    new->next = *head;
-    *head = new;
+	new->n = n;
+	new->next = *head;
+	*head = new;
 
-    return (new);
+	return (new);
 }
 
 /**
@@ -52,12 +53,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
  */
 void free_listint(listint_t *head)
 {
-    listint_t *current;
+	listint_t *current;
 
-    while (head != NULL)
-    {
-        current = head;
-        head = head->next;
-        free(current);
-    }
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current);
+	}
 }
