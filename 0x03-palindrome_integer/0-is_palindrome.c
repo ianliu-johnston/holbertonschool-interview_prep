4126ad1a76671ab676dbfe7944f5acabd719ba17
ianliu-johnston <ian.liu-johnson@holbertonschool.com>
2018-06-07 22:55:00 -0700

    Finishing off test suite main.c file

--- 0x03-palindrome_integer/0-is_palindrome.c
+++ 0x03-palindrome_integer/0-is_palindrome.c
@@ -3,15 +3,6 @@
 #define PALINDROME 1
 #define NOT_PALINDROME 0
 
-unsigned long ex(unsigned long n, unsigned long power)
-{
-	if (power == 0)
-		return (1);
-	for (power-- ; power > 0; power--)
-		n *= 10;
-	return(n);
-}
-
 /**
   * is_palindrome - checks if an integer is a palindrome
   * @n: integer to check
@@ -24,9 +15,9 @@ int is_palindrome(unsigned long n)
 	unsigned long exponent = 1;
 	unsigned long count = 0;
 
-	for(i = n; i > 10; i = i / 10, count++)
+	for (i = n; i >= 10; i = i / 10, count++)
 		exponent *= 10;
-	for(i = count / 2; count > i; count--)
+	for (i = count / 2; count > i; count--)
 	{
 		if (n / exponent != n % 10)
 			return (NOT_PALINDROME);
--- 0x03-palindrome_integer/src/main.c
+++ 0x03-palindrome_integer/src/main.c
@@ -9,17 +9,42 @@
  */
 int main(void)
 {
-#define INT_ARR_LEN 15
 	int index = 0;
-	unsigned long m[INT_ARR_LEN] = {1234554321};
-	unsigned long n[INT_ARR_LEN] = {4774, 47474, 124842, 9842984, 6, 99, 9999999999, 1351678391938761531, 11, 101, 1001, 10001, 1000000000000000001, ULONG_MAX - 1, 0};
+	int is_pal = 0;
+	int success_count = 0;
 
-	printf("0 is%s a palindrome.\n", is_palindrome(0) == 0 ? " NOT" : "");
-	while (n[index] != 0)
+#define ARR_END 20
+	unsigned long test_cases[ARR_END] = {
+		4774, 47474, 124842, 9842984, 9,
+		99, 9999999999, 1351678391938761531, ULONG_MAX - 1, 11,
+		101, 1001, 10001, 1010101, 1000000000000000001,
+		10001001, 1030101, 1000004000001, 10000044000001, 0
+	};
+	int expected[ARR_END] = {
+		1, 1, 0, 0, 1,
+		1, 1, 1, 0, 1,
+		1, 1, 1, 1, 1,
+		0, 0, 1, 1, 1};
+
+	printf("%lu is%s a palindrome.\n",
+			test_cases[ARR_END],
+			is_palindrome(test_cases[ARR_END]) == 0 ? " NOT" : "");
+
+	while (test_cases[index] != test_cases[ARR_END])
 	{
-		printf("%lu is%s a palindrome.\n", n[index], is_palindrome(n[index]) == 0 ? " NOT" : "");
+		printf("CASE: %lu is%s a palindrome.\n",
+				test_cases[index], is_pal == 0 ? " NOT" : "");
+		is_pal = is_palindrome(test_cases[index]);
+		printf("\tResult: %d - Expected: %d = %sCORRECT\033[00m\n\n",
+				is_pal, expected[index], is_pal == expected[index] ? "" : "\033[41mIN");
+		if (is_pal == expected[index])
+			success_count++;
 		index++;
 	}
-	m[0]++;
+	printf("----------------------------------------\n");
+	if (success_count == ARR_END - 1)
+		printf("Tests were successful\n");
+	else
+		printf("\033[41m%d of %d\033[00m Tests were \033[41mNOT\033[00m successful.\n", ARR_END - success_count - 1, ARR_END);
 	return (0);
 }
